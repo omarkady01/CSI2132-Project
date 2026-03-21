ef1679468d865088619ff3a17e7e326cf5acaa19
Omar El Kady <oelka080@uottawa.ca>
2026-03-20 22:40:12 -0400

    fixed search typos connecting to controller and added parameter requests to handle user entered fields in the search page

--- src/main/java/com/eHotel/eHotel/controller/HomeController.java
+++ src/main/java/com/eHotel/eHotel/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.eHotel.eHotel.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -11,7 +12,12 @@ public class HomeController {
     }
 
     @GetMapping("/search")
-    public String searchPage() {
+    public String searchPage(@RequestParam(required = false) String city, @RequestParam(required = false) String capacity, @RequestParam(required = false) Double minPrice, @RequestParam(required = false) Double maxPrice) {
+
+        System.out.println("City: "+city);
+        System.out.println("Capacity: "+capacity);
+        System.out.println("Lowest Price: "+minPrice);
+        System.out.println("Highest Price: "+maxPrice);
         return "search";
     }
 
