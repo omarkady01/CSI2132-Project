7b65bd967e5abf50dc4590f5975349b30e80baf4
Omar El Kady <oelka080@uottawa.ca>
2026-03-20 20:56:46 -0400

    bootstrapping

---
+++ src/test/java/com/eHotel/eHotel/EHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eHotel.eHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
